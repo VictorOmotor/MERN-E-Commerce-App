d72008cd68228ee758657fb115d3a8d1d2dd7c5a
omotorvidosky <omotorvidosky@gmail.com>
2023-10-29 16:31:17 +0100

    Added product  heading component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider';
+import './Home.scss'
+import HomeInfoBox from './HomeInfoBox';
+import PageHeading from '../../components/Home/PageHeading';
+
 
 const Home = () => {
   return (
     <>
       <Slider />
+      <section>
+        <div className="container">
+          <HomeInfoBox />
+          <PageHeading heading={'Latest Products'}
+          btnText={'Shop Now>>>'}/>
+        </div>
+      </section>
     </>
   );
 };
