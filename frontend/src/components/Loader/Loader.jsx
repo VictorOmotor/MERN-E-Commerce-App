dae436222777c317efb379b9366a0d6994359dd9
omotorvidosky <omotorvidosky@gmail.com>
2023-10-31 11:14:11 +0100

    Created and added the loader and spinner components

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Loader.module.scss'
+import ReactDOM from 'react-dom'
+import loaderImg from '../../assets/loader.gif'
+
+const Loader = () => {
+    return ReactDOM.createPortal(
+      <div className={styles.wrapper}>
+        <div className={styles.loader}>
+            <img src={loaderImg} alt="Loading" />
+        </div>
+        </div>,
+        document.getElementById("loader")
+  )
+}
+
+export default Loader
+
+export const Spinner = () => {
+    return (
+       <div className="--center-all">
+        <img src={loaderImg} alt="Loading" width={30} />
+       </div>
+   ) 
+}
